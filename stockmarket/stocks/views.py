460e1e0c478222cd3b1cc381c51a6011f0c36c81
LukeShin3022 <jwshin3022@gmail.com>
2023-10-05 18:44:42 -0700

    stock_market project: taking stock info

--- stockmarket/stocks/views.py
+++ stockmarket/stocks/views.py
@@ -7,7 +7,13 @@ import json
 
 def home(request):
 
-    stock_api = requests.get("https://api.iex.cloud/v1/data/core/quote/aapl?token=pk_15d00f092312459c9a17a01edb0f1e47")
-    stock = json.loads(stock_api.content)
+    try:
+        ticker = request.GET['ticker']
+        stock_api = requests.get("https://api.iex.cloud/v1/data/core/quote/" + ticker + "?token=pk_15d00f092312459c9a17a01edb0f1e47")
+        stock = json.loads(stock_api.content)
+    except Exception as e:
+        stock = ""
+
+
     content = {'stock':stock}
     return render(request,'stocks/home.html',content)
