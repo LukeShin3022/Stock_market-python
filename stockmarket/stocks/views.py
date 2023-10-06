e0fe9cae19b2c2c8bb7a1f60eaa21e510ca1def4
LukeShin3022 <jwshin3022@gmail.com>
2023-10-05 17:48:06 -0700

    stock_market project: create getting stock information with IEX Cloud

--- stockmarket/stocks/views.py
+++ stockmarket/stocks/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+import requests
+import json
 
 # Create your views here.
 
 def home(request):
-    return render(request,'stocks/home.html')
+
+    stock_api = requests.get("https://api.iex.cloud/v1/data/core/quote/aapl?token=pk_15d00f092312459c9a17a01edb0f1e47")
+    stock = json.loads(stock_api.content)
+    content = {'stock':stock}
+    return render(request,'stocks/home.html',content)
