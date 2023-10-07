022d23f0b806ac27dee2b2c5f3ed3ee5f109a769
LukeShin3022 <jwshin3022@gmail.com>
2023-10-06 19:04:35 -0700

    Stock_market proj: complete overview page

--- stockmarket/stocks/dashboard_e.py
+++ stockmarket/stocks/dashboard_e.py
@@ -1,25 +1,18 @@
 import streamlit as st
 import requests
 import config_e
-
-
+from iex_e import IEXStock
 
 symbol = st.sidebar.text_input("symbol", value="AAPL")
+stock = IEXStock(config_e.IEX_TOKEN, symbol)
 
 screen = st.sidebar.selectbox("View", ('Overview', 'Fundamentals', 'News', 'Ownership'))
 st.title(screen)
 
 
 if screen == 'Overview':
-    url = f"https://api.iex.cloud/v1/stock/{symbol}/logo?token={config_e.IEX_TOKEN}"
-    r = requests.get(url)
-    logo = r.json()
-    # response_json = response_json[0]
-
-    url = f"https://api.iex.cloud/v1/data/core/company/{symbol}?token={config_e.IEX_TOKEN}"
-    r = requests.get(url)
-    response_json = r.json()
-    response_json = response_json[0]
+    logo = stock.get_logo()
+    company_info = stock.get_company_info()[0]
 
     col1, col2 = st.columns([1, 4])
 
@@ -27,12 +20,12 @@ if screen == 'Overview':
         st.image(logo['url'])
 
     with col2:
-        st.subheader(response_json['companyName'])
+        st.subheader(company_info['companyName'])
         st.subheader('Description')
-        st.write(response_json['longDescription'])
+        st.write(company_info['longDescription'])
         st.subheader('Industry')
-        st.write(response_json['industry'])
+        st.write(company_info['industry'])
         st.subheader('CEO')
-        st.write(response_json['ceo'])
+        st.write(company_info['ceo'])
 
 
--- stockmarket/stocks/iex_e.py
+++ stockmarket/stocks/iex_e.py
@@ -1,10 +1,11 @@
 import requests
+import config_e
 
 class IEXStock:
 
     def __init__(self, token, symbol, environment='production'):
         if environment == 'production':
-            self.BASE_URL = 'https://cloud.iexapis.com/v1'
+            self.BASE_URL = 'https://api.iex.cloud/v1'
         else:
             self.BASE_URL = 'https://sandbox.iexapis.com/v1'
         
@@ -12,7 +13,14 @@ class IEXStock:
         self.symbol = symbol
 
     def get_logo(self):
-        url = f"{self.BASE_URL}/stock/{self.symbol}/logo?token={self.token}"
+        url = f"{self.BASE_URL}/stock/{self.symbol}/logo?token={config_e.IEX_TOKEN}"
         r = requests.get(url)
 
         return r.json()
+
+    def get_company_info(self):
+        url = f"{self.BASE_URL}/data/core/company/{self.symbol}?token={config_e.IEX_TOKEN}"
+        r = requests.get(url)
+
+        return r.json()
+
